29352ca6efb8425c0fe539f10c6a26946104ce42
normaliner <prokopenko.viktor110701@mail.ru>
2023-12-13 22:53:23 +0300

    Completed

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,24 @@
 
 let habbits = [];
 const HABBIT_KEY = 'HABBIT_KEY';
+let globalHabbitId;
 
 /* page */
 const page = {
   menu: document.querySelector('.menu__list'),
+  header: {
+    h1: document.querySelector('.h1'),
+    progressPercent: document.querySelector('.progress__percent'),
+    progressCoverBar: document.querySelector('.progress__cover-bar'),
+  },
+  content: {
+    daysContainer: document.querySelector('.days'),
+    nextDay: document.querySelector('.habbit__day'),
+  },
+  popup: {
+    index: document.getElementById('add-habbit-popup'),
+    iconFiled: document.querySelector('.popup__form input[name="icon"]'),
+  },
 };
 
 /* utils */
@@ -21,11 +35,46 @@ function saveData() {
   localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
 }
 
-/* render */
-function rerenderMenu(activeHabbit) {
-  if (!activeHabbit) {
+function togglePopup() {
+  if (page.popup.index.classList.contains('cover_hidden')) {
+    page.popup.index.classList.remove('cover_hidden');
+  } else {
+    page.popup.index.classList.add('cover_hidden');
+  }
+}
+
+function resetForm(form, fields) {
+  for (const field of fields) {
+    form[field].value = '';
+  }
+}
+
+function validateAndGetFormData(form, fields) {
+  const formData = new FormData(form);
+  const res = {};
+  for (const field of fields) {
+    const fieldValue = formData.get(field);
+    form[field].classList.remove('error');
+    if (!fieldValue) {
+      form[field].classList.add('error');
+      return;
+    }
+    res[field] = fieldValue;
+  }
+  let isValid = true;
+  for (const field of fields) {
+    if (!res[field]) {
+      isValid = false;
+    }
+  }
+  if (!isValid) {
     return;
   }
+  return res;
+}
+
+/* render */
+function rerenderMenu(activeHabbit) {
   for (const habbit of habbits) {
     const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
 
@@ -36,6 +85,7 @@ function rerenderMenu(activeHabbit) {
       element.classList.add('menu__item');
       element.addEventListener('click', () => rerender(habbit.id));
       element.innerHTML = `<img src="images/${habbit.icon}.svg" alt="${habbit.name}" />`;
+
       if (activeHabbit.id === habbit.id) {
         element.classList.add('menu__item_active');
       }
@@ -50,13 +100,128 @@ function rerenderMenu(activeHabbit) {
   }
 }
 
+function rerenderHead(activeHabbit) {
+  page.header.h1.innerText = activeHabbit.name;
+  const progress =
+    activeHabbit.days.length / activeHabbit.target > 1
+      ? 100
+      : (activeHabbit.days.length / activeHabbit.target) * 100;
+  page.header.progressPercent.innerText = progress.toFixed() + '%';
+  page.header.progressCoverBar.setAttribute('style', `width: ${progress}%`);
+}
+
+function rerenderContent(activeHabbit) {
+  page.content.daysContainer.innerHTML = '';
+  for (const index in activeHabbit.days) {
+    const element = document.createElement('div');
+    element.classList.add('habbit');
+    element.innerHTML = `<div class="habbit__day">День ${
+      Number(index) + 1
+    }</div>
+    <div class="habbit__comment">
+      ${activeHabbit.days[index].comment}
+    </div>
+    <button class="habbit__delete">
+      <img src="images/delete.svg" alt="Удалить"  onclick="deleteDay(${index})" />
+    </button>
+    `;
+    page.content.daysContainer.appendChild(element);
+  }
+  page.content.nextDay.innerHTML = `День ${activeHabbit.days.length + 1}`;
+}
+
 function rerender(activeHabbitId) {
+  globalHabbitId = activeHabbitId;
   const activeHabbit = habbits.find((habbit) => habbit.id === activeHabbitId);
+  if (!activeHabbit) {
+    return;
+  }
+  console.log(activeHabbitId);
+  document.location.replace(document.location.pathname + '#' + activeHabbitId);
   rerenderMenu(activeHabbit);
+  rerenderHead(activeHabbit);
+  rerenderContent(activeHabbit);
+}
+
+/* work with days */
+function addDays(event) {
+  event.preventDefault();
+  const data = validateAndGetFormData(event.target, ['comment']);
+  if (!data) {
+    return;
+  }
+  habbits = habbits.map((habbit) => {
+    if (habbit.id === globalHabbitId) {
+      return {
+        ...habbit,
+        days: habbit.days.concat([{ comment: data.comment }]),
+      };
+    }
+    return habbit;
+  });
+
+  resetForm(event.target, ['comment']);
+  rerender(globalHabbitId);
+  saveData();
+}
+
+function deleteDay(index) {
+  habbits = habbits.map((habbit) => {
+    if (habbit.id === globalHabbitId) {
+      habbit.days.splice(index, 1);
+      return {
+        ...habbit,
+        days: habbit.days,
+      };
+    }
+    return habbit;
+  });
+  rerender(globalHabbitId);
+  saveData();
+}
+
+/* working with habbits */
+function setIcon(context, icon) {
+  page.popup.iconFiled.value = icon;
+  const activeIcon = document.querySelector('.icon.icon_active');
+  activeIcon.classList.remove('icon_active');
+  context.classList.add('icon_active');
+}
+
+function addHabbit(event) {
+  event.preventDefault();
+  const data = validateAndGetFormData(event.target, ['name', 'icon', 'target']);
+  if (!data) {
+    return;
+  }
+  const maxId = habbits.reduce(
+    (acc, habbit) => (acc > habbit.id ? acc : habbit.id),
+    0
+  );
+  habbits.push({
+    id: maxId + 1,
+    name: data.name,
+    target: data.target,
+    icon: data.icon,
+    days: [],
+  });
+  resetForm(event.target, ['name', 'target']);
+  togglePopup();
+  saveData();
+  rerender(maxId + 1);
 }
 
 /* init */
 (() => {
   loadData();
-  rerender(habbits[0].id);
+  const hashId = Number(document.location.hash.replace('#', ''));
+  console.log(hashId);
+  const urlHabbit = habbits.find((habbit) => (habbit.id = hashId));
+  console.log(urlHabbit);
+  if (urlHabbit) {
+    rerender(urlHabbit.id);
+  } else {
+    rerender(habbits[0].id);
+    rerender(habbits[1].id);
+  }
 })();
